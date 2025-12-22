6de80fbb9a9cb9b4ef4e69d4c1b682878fa141fd
Safeer Ahmad Rana <pakistanireacts9@gmail.com>
2025-12-22 14:45:42 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,28 @@
+<<<<<<< Updated upstream
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './styles/main.scss'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+=======
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import "./styles/main.scss";
+>>>>>>> Stashed changes
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+<<<<<<< Updated upstream
   </StrictMode>,
 )
+=======
+  </StrictMode>
+);
+>>>>>>> Stashed changes
